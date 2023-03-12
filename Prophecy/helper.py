6e76af897a3d354111e59e214e11d48cb1f46b72
JinJin <codingjinjin@gmail.com>
2023-03-12 12:47:43 -0500

    refactor all the tables. Thanks Mike:)

--- Prophecy/helper.py
+++ Prophecy/helper.py
@@ -3,34 +3,31 @@ from cs50 import SQL
 
 db = SQL("sqlite:///roster.db")
 
-students = []
-houses = []
-heads = []
-assignment = []
 with open ("students.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
+        # Get student name, house name and head name
         name = row["student_name"]
         house = row["house"]
         head = row["head"]
-        students.append({"student_name": name})
-        assignment.append({"student_name": name, "house":house})
-        house_exists = False
-        for dict in houses:
-            if house in dict.values():
-                house_exists = True
-                break
-        if not house_exists:
-            new_house = {"house": house, "head": head}
-            houses.append(new_house)
-#print(assignment)
-#print(students)
-#print(houses)
 
-for student in students:
-    db.execute("INSERT INTO students(student_name) VALUES (?)", student["student_name"])
+        # Insert student name into student table
+        db.execute("INSERT INTO students (student_name) VALUES (?)", name)
+        student_id_list = db.execute("SELECT id FROM students WHERE student_name =?", name)
+        student_id = student_id_list[0]["id"]
 
-for house in houses:
-    db.execute("INSERT INTO houses(house_name, house_head) VALUES (?, ?)", house["house"], house["head"])
+        # Check if house exists in house table
+        result = db.execute("SELECT id FROM houses WHERE house_name = ?", house)
 
-# need to figure out a way to insert house_id and student_id from tables to the assignment table still 
+        # check if the result list is empty
+        if len(result) != 0:
+            house_id = result[0]["id"]
+        elif len(result) == 0:
+            db.execute("INSERT INTO houses (house_name, house_head) VALUES(?, ?)", house, head)
+            house_id_list = db.execute("SELECT id FROM houses WHERE house_name = ?", house)
+            house_id = house_id_list[0]["id"]
+
+
+        # Insert both student and house ids into assignment table
+        db.execute("INSERT INTO assignment(student_id, house_id) VALUES(?, ?)", student_id, house_id)
+
